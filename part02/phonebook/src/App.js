0a9bc3db8db867d615faa7678e39205620ca189f
dynamo <hietanenelias@gmail.com>
2022-09-12 11:03:07 +0300

    exercise 2.8

--- part02/phonebook/src/App.js
+++ part02/phonebook/src/App.js
@@ -3,22 +3,24 @@ import _ from "lodash"
 
 const Person = (props) => {
   return(
-    <p>{props.name}</p>
+    <p>{props.person.name} {props.person.number}</p>
   )
 }
 
 const App = () => {
 
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', number: '040-1234567' }
   ]) 
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
   const handleNameChange = (event) => setNewName(event.target.value)
+  const handleNumberChange = (event) => setNewNumber(event.target.value)
   
-  const addName = (event) => {
+  const addEntry = (event) => {
     event.preventDefault()
-    const personObject = {name : newName}
+    const personObject = {name : newName, number: newNumber}
     if(persons.filter(person => _.isEqual(personObject, person)).length === 0 ){
       setPersons(persons.concat(personObject))
     }else{
@@ -26,21 +28,25 @@ const App = () => {
     }
     
     setNewName("")
+    setNewNumber("")
     
   }
   return (
     <div>
       <h2>Phonebook</h2>
-      <form onSubmit={addName}>
+      <form onSubmit={addEntry}>
         <div>
           name: <input value={newName} onChange={handleNameChange}/>
+        </div>
+        <div>
+          number: <input value={newNumber} onChange={handleNumberChange}/> 
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map(person => <Person key={person.name} name={person.name}/>)}
+      {persons.map(person => <Person key={person.name} person={person}/>)}
     </div>
   )
 }
--- part02/phonebook/src/readme.txt
+++ part02/phonebook/src/readme.txt
@@ -1,3 +1,12 @@
 *** exercice 2.7
 I Could've used JSON.stringify method to convert the objects to strings and then compare them with ===,
 but I figured the order in which the properties are defined within the objects shouldn't matter. So lodash it is.
+
+*** exercise 2.8
+I didn't write any safeguards on the app (empty name, empty number etc...). Since there were no specific requirements
+other than not having the same name multiple times, I thought the user should be able to enter blank entries.
+Another thing to keep in mind, with this implementation, the user is able to make several entries with the same name,
+but different phone numbers (and vice versa althought this is arguably wrong since there shouldn't be multiple individuals
+using the same number except for non-mobile phones which tend to disapear nowadays haha), which should be fine in the case 
+of multiple sim cards for example. 
+Also I could've used the phone number as the key instead of the name, because the number is indeed unique.
