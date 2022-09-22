d5ccaa055b44c19900c3c9671c9f5ac40a04f682
dynamo <hietanenelias@gmail.com>
2022-09-22 22:45:26 +0300

    exercise 4.5

--- part04/blog-list-backend/utils/list_helper.js
+++ part04/blog-list-backend/utils/list_helper.js
@@ -6,8 +6,14 @@ const totalLikes = (blogs) => {
   return blogs.reduce((sum, blog) => {
     return sum + blog.likes}, 0)
 }
+
+const favoriteBlog = (blogs) => {
+  return blogs.length !== 0 ? 
+    blogs.reduce((top, b) => {return b.likes > top.likes ? b : top}, blogs[0]) : null
+}
   
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }
