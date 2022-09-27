7aaae1fea4407e0d1bebb21340981c08739dce6e
dynamo <hietanenelias@gmail.com>
2022-09-28 00:23:13 +0300

    exercise 4.21

--- part04/blog-list-backend/controllers/blogs.js
+++ part04/blog-list-backend/controllers/blogs.js
@@ -3,6 +3,7 @@ const { request } = require('../app')
 const Blog = require('../models/blog')
 const User = require('../models/user')
 const jwt = require('jsonwebtoken')
+const blog = require('../models/blog')
 
 blogsRouter.get('/', async (request, response) => {
 		const blogs = await Blog.find({}).populate('user', { username: 1, name: 1, id: 1 })
@@ -23,12 +24,25 @@ blogsRouter.post('/', async (request, response) => {
   user.blogs = user.blogs.concat(returnedBlog._id)
   await user.save()
   response.status(201).json(returnedBlog)
- 
 })
 
 blogsRouter.delete('/:id', async (request, response) => {
-  await Blog.findByIdAndRemove(request.params.id)
-  response.status(204).end()
+  const blogToRemove = await Blog.findById(request.params.id)
+  const decodedToken = jwt.verify(request.token, process.env.SECRET)
+  if (!decodedToken.id) {
+    return response.status(401).json({ error: 'token missing or invalid' })
+  }
+  if(!blogToRemove){
+    response.status(400).json({ error: 'malformatted id' })
+  }else if(decodedToken.id.toString() === blogToRemove.user.toString()){
+    let user = await User.findById(decodedToken.id)
+    user.blogs = user.blogs.filter(id => id.toString() !== blogToRemove._id.toString())
+    await user.save()
+    await blogToRemove.remove()
+    response.status(204).end()
+  }else{    
+    response.status(401).json({ error: "you can't delete blogs you don't own"})
+  }
 })
 
 blogsRouter.get('/:id', async (request, response) => {
