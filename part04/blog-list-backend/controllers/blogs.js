65e46a2e52b86c54695d7e7f08f8fd284e4479a7
dynamo <hietanenelias@gmail.com>
2022-09-27 21:51:06 +0300

    exercise 4.19

--- part04/blog-list-backend/controllers/blogs.js
+++ part04/blog-list-backend/controllers/blogs.js
@@ -2,6 +2,15 @@ const blogsRouter = require('express').Router()
 const { request } = require('../app')
 const Blog = require('../models/blog')
 const User = require('../models/user')
+const jwt = require('jsonwebtoken')
+
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    return authorization.substring(7)
+  }
+  return null
+}
 
 blogsRouter.get('/', async (request, response) => {
 		const blogs = await Blog.find({}).populate('user', { username: 1, name: 1, id: 1 })
@@ -10,8 +19,14 @@ blogsRouter.get('/', async (request, response) => {
 })
 
 blogsRouter.post('/', async (request, response) => {
-  const postedBlog = request.body
-  const user = await User.findById(postedBlog.user)
+  let postedBlog = request.body
+  const token = getTokenFrom(request)
+  const decodedToken = jwt.verify(token, process.env.SECRET)
+  if (!decodedToken.id) {
+    return response.status(401).json({ error: 'token missing or invalid' })
+  }
+  const user = await User.findById(decodedToken.id)
+  postedBlog = {...postedBlog, user: user._id}
   const blog = new Blog(postedBlog)
   let returnedBlog = await blog.save()
   user.blogs = user.blogs.concat(returnedBlog._id)
