7717140b31df21c1dd1e215521b15074b49e9824
dynamo <hietanenelias@gmail.com>
2022-10-04 15:34:18 +0300

    added owner's name in the detailed view of the blog - exercise 5.7

--- part05/bloglist-frontend/src/components/Blog.js
+++ part05/bloglist-frontend/src/components/Blog.js
@@ -25,6 +25,7 @@ const Blog = ({blog}) => {
         <div>{blog.title} {blog.author}<button onClick={toggleDetailedView}>{buttonLabel}</button></div>
         <div>{blog.url}</div>
         <div>{blog.likes}<button>like</button></div>
+        <div>{blog.user.name}</div>
       </div>
     )
   }
