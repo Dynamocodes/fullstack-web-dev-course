bf6a4df0e71da8aa6210606a0e0f7fdc2e5dbc39
dynamo <hietanenelias@gmail.com>
2022-11-22 18:07:23 +0200

    exercise 8.17-8.18

--- part08/library-frontend/src/App.js
+++ part08/library-frontend/src/App.js
@@ -1,10 +1,51 @@
 import { useState } from 'react'
+import { useApolloClient } from '@apollo/client'
 import Authors from './components/Authors'
 import Books from './components/Books'
 import NewBook from './components/NewBook'
+import LoginForm from './components/LoginForm'
+import Notify from './components/Notify'
 
 const App = () => {
   const [page, setPage] = useState('authors')
+  const [token, setToken] = useState(null)
+  const [errorMessage, setErrorMessage] = useState(null)
+  const client = useApolloClient()
+
+  const notify = (message) => {
+    setErrorMessage(message)
+    setTimeout(() => {
+      setErrorMessage(null)
+    }, 10000)
+  }
+
+  const logout = () => {
+    setToken(null)
+    localStorage.clear()
+    client.resetStore()
+    setPage('login')
+  }
+
+  if (!token) {
+    return (
+      <div>
+        <Notify errorMessage={errorMessage} />
+        <div>
+          <button onClick={() => setPage('authors')}>authors</button>
+          <button onClick={() => setPage('books')}>books</button>
+          <button onClick={() => setPage('login')}>login</button>
+        </div>
+
+        <Authors show={page === 'authors'} />
+
+        <Books show={page === 'books'} />
+
+        <LoginForm show={page === 'login'} setToken={setToken} setError={notify} setPage={setPage}/>
+      </div>
+    )
+  }
+
+
 
   return (
     <div>
@@ -12,6 +53,7 @@ const App = () => {
         <button onClick={() => setPage('authors')}>authors</button>
         <button onClick={() => setPage('books')}>books</button>
         <button onClick={() => setPage('add')}>add book</button>
+        <button onClick={() => logout()}>logout</button>
       </div>
 
       <Authors show={page === 'authors'} />
@@ -19,6 +61,7 @@ const App = () => {
       <Books show={page === 'books'} />
 
       <NewBook show={page === 'add'} />
+
     </div>
   )
 }
