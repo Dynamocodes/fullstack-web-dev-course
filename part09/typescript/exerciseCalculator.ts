11982cce840e84d4713b2eb5ee2aac7d5d87e67e
dynamo <hietanenelias@gmail.com>
2022-12-04 17:44:52 +0200

    exercise 9.3

--- part09/typescript/exerciseCalculator.ts
+++ part09/typescript/exerciseCalculator.ts
@@ -8,7 +8,29 @@ interface Result {
     average: number
 }
 
+interface ExerciseValues {
+    days: Array<number>,
+    target: number
+}
+
+const parseArgumentsExercise = (args: Array<string>) : ExerciseValues => {
+    if (args.length < 4) throw new Error('Not enough arguments');
+    let daysString = args.filter((e, i ) => {return i >= 3})
+    let daysNumber = daysString.map(e => {
+        if(isNaN(Number(e))) throw new Error('Provided values were invalid!')
+        return Number(e)
+    })
+    if (!isNaN(Number(args[2]))) {
+        return {
+          days: daysNumber,
+          target: Number(args[2])
+        }
+      }
+}
+
 const MAX_RATNIG = 5
+
+
 const calculateExercises = (days: Array<number>, target: number): Result => {
     const average = days.reduce((amount, hours) => {return amount + hours}, 0)/days.length
     let rating
@@ -32,7 +54,7 @@ const calculateExercises = (days: Array<number>, target: number): Result => {
         case rating < MAX_RATNIG * 0.8:
             ratingDescription = "really good work, a tiny bit more work and you'd get full credit"
             break
-        case rating < MAX_RATNIG :
+        case rating <= MAX_RATNIG :
             ratingDescription = "excellent, job done as expected"
             break
         default:
@@ -53,4 +75,15 @@ const calculateExercises = (days: Array<number>, target: number): Result => {
     return result
 }
 
-console.log(calculateExercises([3, 0, 2, 4.5, 0, 3, 1], 2))
+try {
+    const { days, target } = parseArgumentsExercise(process.argv);
+    console.log(calculateExercises(days, target))
+  } catch (error: unknown) {
+    let errorMessage = 'Something bad happened.'
+    if (error instanceof Error) {
+      errorMessage += ' Error: ' + error.message;
+    }
+    console.log(errorMessage);
+  }
+
+//console.log(calculateExercises([3, 0, 2, 4.5, 0, 3, 1], 2))
