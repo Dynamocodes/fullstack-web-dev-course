44e366e735857740c0689866cf789f5a6b8ec77a
dynamo <hietanenelias@gmail.com>
2022-12-25 14:07:11 +0200

    exercise 9.22

--- part09/patientor-frontend/src/components/EntryView.tsx
+++ part09/patientor-frontend/src/components/EntryView.tsx
@@ -1,25 +1,75 @@
-import { Diagnosis, Entry } from "../types";
+import { Diagnosis, Entry, HealthCheckEntry, OccupationalHealthcareEntry, HospitalEntry } from "../types";
+import MedicalServicesIcon from '@mui/icons-material/MedicalServices';
+import WorkRoundedIcon from '@mui/icons-material/WorkRounded';
+import LocalHospitalRoundedIcon from '@mui/icons-material/LocalHospitalRounded';
+
+const getDiagnosisDesc = (code: Diagnosis['code'], diagnoses : Diagnosis[]) => {
+  return diagnoses.find(diagnosis => diagnosis.code === code);
+};
+
+const getDiagnosisName = (diagnosis : Diagnosis | undefined) => {
+  return diagnosis ? diagnosis.name : "";
+};
+
+const OccupationalHealthCareView = ({entry, diagnoses} : {entry: OccupationalHealthcareEntry, diagnoses:Diagnosis[]}) => {
+  const diagnosesList = entry.diagnosisCodes 
+  ? <ul>{entry.diagnosisCodes.map(code => <li key={code}>{code} : {getDiagnosisName(getDiagnosisDesc(code,diagnoses ))}</li>)}</ul> 
+  : null;
+
+  return (
+    <div className="entry">
+      <div><WorkRoundedIcon/> : {entry.employerName}</div>
+      {entry.date} <i>{entry.description}</i>
+      {diagnosesList}
+      <div>Diagnosed by : {entry.specialist}</div>
+    </div>
+  );
+};
+
+const HealthCheckView = ({entry, diagnoses} : {entry: HealthCheckEntry, diagnoses:Diagnosis[]}) => {
+
+  const diagnosesList = entry.diagnosisCodes 
+  ? <ul>{entry.diagnosisCodes.map(code => <li key={code}>{code} : {getDiagnosisName(getDiagnosisDesc(code,diagnoses ))}</li>)}</ul> 
+  : null;
+
+  return (
+    <div className="entry">
+      <div><MedicalServicesIcon/></div>
+      {entry.date} <i>{entry.description}</i>
+      {diagnosesList}
+      <div>Diagnosed by : {entry.specialist}</div>
+    </div>
+  );
+};
+
+const HospitalView = ({entry, diagnoses} : {entry: HospitalEntry, diagnoses:Diagnosis[]}) => {
+
+  const diagnosesList = entry.diagnosisCodes 
+  ? <ul>{entry.diagnosisCodes.map(code => <li key={code}>{code} : {getDiagnosisName(getDiagnosisDesc(code,diagnoses ))}</li>)}</ul> 
+  : null;
+
+  return (
+    <div className="entry">
+      <div><LocalHospitalRoundedIcon/></div>
+      {entry.date} <i>{entry.description}</i>
+      {diagnosesList}
+      <div>Discharged the: {entry.discharge.date}</div>
+      <div>under the following condition: {entry.discharge.criteria}</div>
+      <div>Diagnosed by : {entry.specialist}</div>
+    </div>
+  );
+};
 
 const EntryView = ({entry, diagnoses} : {entry: Entry, diagnoses:Diagnosis[]}) => {
      
-    
-    const getDiagnosisDesc = (code: Diagnosis['code']) => {
-        return diagnoses.find(diagnosis => diagnosis.code === code);
-    };
-
-    const getDiagnosisName = (diagnosis : Diagnosis | undefined) => {
-        return diagnosis ? diagnosis.name : "";
-    };
-
-    const diagnosesList = entry.diagnosisCodes 
-    ? <ul>{entry.diagnosisCodes.map(code => <li key={code}>{code} : {getDiagnosisName(getDiagnosisDesc(code))}</li>)}</ul> 
-    : null;
-    return(
-        <div>
-            {entry.date} <i>{entry.description}</i>
-            {diagnosesList}
-        </div>
-    );     
+  switch(entry.type){
+    case "Hospital":
+      return <HospitalView entry={entry} diagnoses={diagnoses}/>;
+    case "HealthCheck":
+      return <HealthCheckView entry={entry} diagnoses={diagnoses}/>;
+    case "OccupationalHealthCare":
+      return <OccupationalHealthCareView entry={entry} diagnoses={diagnoses}/>;
+  }  
    
 };
 
