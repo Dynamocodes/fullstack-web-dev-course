a2167dcc8270f27d3049a4569f196633b0fdd620
dynamo <hietanenelias@gmail.com>
2023-04-23 22:26:23 +0200

    adding pokedex repository and my own automated app to part 11

---
+++ part11/blog-list/backend/index.js
@@ -0,0 +1,11 @@
+const app = require('./app.js')
+const http = require('http')
+const config = require('./utils/config')
+const {info, error} = require('./utils/logger')
+
+
+
+const PORT = config.PORT
+app.listen(PORT, () => {
+  info(`Server running on port ${PORT}`)
+})
---
+++ part11/blog-list/frontend/src/components/LoginForm.js
@@ -0,0 +1,47 @@
+import PropTypes from 'prop-types'
+
+const LoginForm = (
+  {
+    handleLogin,
+    handleUsernameChange,
+    usernameValue,
+    handlePasswordChange,
+    passwordValue
+  }) => {
+  return(
+    <div>
+      <h1>Log in to application</h1>
+      <form onSubmit={handleLogin}>
+        <div>
+          username
+          <input
+            id='username'
+            type='text'
+            onChange={handleUsernameChange}
+            value={usernameValue}
+            name='Username'/>
+        </div>
+        <div>
+          password
+          <input
+            id='password'
+            type='password'
+            onChange={handlePasswordChange}
+            value={passwordValue}
+            name='Password'/>
+        </div>
+        <button id='loginButton' type="submit">login</button>
+      </form>
+    </div>
+  )
+}
+
+LoginForm.propTypes = {
+  handleLogin: PropTypes.func.isRequired,
+  handleUsernameChange: PropTypes.func.isRequired,
+  usernameValue: PropTypes.string.isRequired,
+  handlePasswordChange: PropTypes.func.isRequired,
+  passwordValue: PropTypes.string.isRequired
+}
+
+export default LoginForm
