66a36f71f9ee8bdf354a50735d7c07e848d5ccaf
dynamo <hietanenelias@gmail.com>
2023-05-09 14:30:25 +0200

    exercise 12.10

--- part12/containers-applications/todo-app/todo-backend/routes/todos.js
+++ part12/containers-applications/todo-app/todo-backend/routes/todos.js
@@ -1,10 +1,11 @@
 const express = require('express');
-const { Todo } = require('../mongo')
+const { Todo } = require('../mongo');
+const { setAsync, getAsync } = require('../redis');
 const router = express.Router();
 
 /* GET todos listing. */
 router.get('/', async (_, res) => {
-  const todos = await Todo.find({})
+  const todos = await Todo.find({});
   res.send(todos);
 });
 
@@ -13,23 +14,35 @@ router.post('/', async (req, res) => {
   const todo = await Todo.create({
     text: req.body.text,
     done: false
-  })
+  });
+
+  // Increment the todo counter
+  const currentCount = await getAsync('added_todos');
+  const newCount = currentCount ? parseInt(currentCount) + 1 : 1;
+  await setAsync('added_todos', newCount);
+
   res.send(todo);
+});
+
+/* GET todo statistics. */
+router.get('/statistics', async (_, res) => {
+  const addedTodos = await getAsync('added_todos') || '0';
+  res.send({ added_todos: parseInt(addedTodos) });
 });
 
 const singleRouter = express.Router();
 
 const findByIdMiddleware = async (req, res, next) => {
-  const { id } = req.params
-  req.todo = await Todo.findById(id)
-  if (!req.todo) return res.sendStatus(404)
+  const { id } = req.params;
+  req.todo = await Todo.findById(id);
+  if (!req.todo) return res.sendStatus(404);
 
-  next()
-}
+  next();
+};
 
 /* DELETE todo. */
 singleRouter.delete('/', async (req, res) => {
-  await req.todo.delete()  
+  await req.todo.delete();
   res.sendStatus(200);
 });
 
@@ -47,6 +60,6 @@ singleRouter.put('/', async (req, res) => {
   res.send(req.todo);
 });
 
-router.use('/:id', findByIdMiddleware, singleRouter)
+router.use('/:id', findByIdMiddleware, singleRouter);
 
 module.exports = router;
